2050068e38caa0d23805efaf98f365e0d37eb1f9
Jay Lawrence <jay@patientway.com>
2017-06-09 22:36:48 -0400

    add browser_sync.js config item feature

--- lib/browser_sync_rails/runner.rb
+++ lib/browser_sync_rails/runner.rb
@@ -5,7 +5,7 @@ module BrowserSyncRails
     end
 
     def cli_command
-      "browser-sync start --proxy #{host}:#{port} --files '#{files}'"
+      "browser-sync start --proxy #{host}:#{port} --files '#{files}' #{config}"
     end
 
     private
@@ -24,6 +24,15 @@ module BrowserSyncRails
         (files | @options[:files]).join(', ')
       else
         files.join(', ')
+      end
+    end
+
+    def config
+      config_file = @options[:config] || File.join(Rails.root, 'config', 'browser_sync.js')
+      if File.exist?(config_file)
+        "--config #{config_file}"
+      else
+        ""
       end
     end
   end
